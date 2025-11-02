0c325cd772043b1f0e583bac731bceb4937f7ad6
BlastBlastBlast <lw.tynes@gmail.com>
2025-11-02 15:07:36 +0100

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Coupler-Mock/' : '/',
+  base: command === 'build' ? '/Coupler-Mock/' : '/',
   server: {
     port: 3000
   }
-})
+}))
 
